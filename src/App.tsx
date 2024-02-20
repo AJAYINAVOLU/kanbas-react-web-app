1f329d66c3b3f9add1adb065f20f84858f97014c
AJAY <inavolu.a@northeastern.edu>
2024-02-19 19:58:58 -0500

    A3_Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import './index.css';
-import Labs from "./Labs";
+import Labs from "./Labs";      // Imports ./Labs/index.tsx.
 import HelloWorld from "./Labs/a3/HelloWorld";
-import Kanbas from "./Kanbas";
-import {HashRouter} from "react-router-dom";
-import {Routes, Route, Navigate} from "react-router";
-
+import Kanbas from "./Kanbas";       // This import statement will first attempt to import a file called Kanbas.tsx, and if it fails it will then attempt to import index.tsx in a folder called Kanbas, e.g., Kanbas/index.tsx, which is what we have here.
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return (
-      <HashRouter>
-          <div>
-              <Routes>
-                  <Route path="/"         element={<Navigate to="/Labs"/>}/>
-                  <Route path="/Labs/*"   element={<Labs/>}/>
-                  <Route path="/Kanbas/*" element={<Kanbas/>}/>
-                  <Route path="/hello"    element={<HelloWorld/>}/>
-              </Routes>
-          </div>
-      </HashRouter>
-
-  );
+    return (                    // Return can only return a single component. That is why we wrap the whole content with a <div>.
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={ <Navigate to="Labs/a3"/>}></Route>
+                <Route path="/Labs/*" element={<Labs/>}></Route>
+                <Route path="/Kanbas/*" element={<Kanbas/>}></Route>
+                <Route path="/hello" element={<HelloWorld/>}></Route>
+            </Routes>
+        </HashRouter>
+    );
 }
 export default App;
-
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,20 +1,16 @@
+// The navigation links in the three components: Labs, HelloWorld, and Kanbas.
+// It is best implemented as a reusable component, as shown below.
+// This component can be imported into the HelloWorld, Labs, and Kanbas components.
 import { Link, useLocation } from "react-router-dom";
+
 function Nav() {
     const { pathname } = useLocation();
-    return (
-        // <nav className="nav nav-tabs mt-2">
-        //     <Link className="nav-link" to="/Labs/a3">A3</Link>
-        //     <Link className="nav-link" to="/Kanbas">Kanbas</Link>
-        //     <Link className="nav-link" to="/hello">Hello</Link>
-        // </nav>
+    return(
         <nav className="nav nav-tabs mt-2">
-            <Link to="/Labs/a3"
-                  className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}>A3</Link>
-            <Link to="/Kanbas"
-                  className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}>Kanbas</Link>
-            <Link to="/hello"
-                  className={`nav-link ${pathname.includes("hello") ? "active" : ""}`}>Hello</Link>
+            <Link to="/Labs/a3" className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}>A3</Link>
+            <Link to="/Kanbas"className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}>Kanbas</Link>
+            <Link to="/hello" className={`nav-link ${pathname.includes("hello") ? "active" : ""}`}>Hello</Link>
         </nav>
     );
 }
-export default Nav;
+export default Nav
